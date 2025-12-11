0830b1b1fb607ff901d4ee283ce1f7000a81be98
rushin-gh <rushin.mailme@gmail.com>
2025-12-11 11:06:50 +0530

    Login and user creation

--- database/firstscript.sql
+++ database/firstscript.sql
@@ -1,3 +1,10 @@
 --create database JobTracker
 use JobTracker
 
+-- For creating user in database
+create Login app_login with password = 'app_password'
+create user app_user for Login app_login
+
+alter role db_datareader add member app_user
+alter role db_datawriter add member app_user
+alter role db_ddladmin add member app_user
